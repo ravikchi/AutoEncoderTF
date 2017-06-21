ad8f4619150be10ce80bcd4f42a3e17f0d2771a8
Chimatapu <ravikchi@gmail.com>
2017-06-21 13:11:13 +0100

    a few changes

--- ae.py
+++ ae.py
@@ -35,14 +35,14 @@ biases = {
 def encoder(x):
     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
                                    biases['encoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
-                                   biases['encoder_b2']))
-    return layer_2
+    #layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+     #                              biases['encoder_b2']))
+    return layer_1
 
 def decoder(x):
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
-                                   biases['decoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+    #layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+     #                              biases['decoder_b1']))
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h2']),
                                    biases['decoder_b2']))
     return layer_2
 
