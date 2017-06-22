e2e5d2eafafdb5d8d2f249feb45e104bd75b604d
Chimatapu <ravikchi@gmail.com>
2017-06-22 11:21:45 +0100

    working code

--- aeTest.py
+++ aeTest.py
@@ -4,14 +4,13 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-learning_rate = 0.01
-training_epochs = 20
-batch_size = 256
-display_step = 1
-examples_to_show = 10
+def runLayers(input_data, layers, sess):
+    opt = input_data
+    for i in range(len(layers)):
+        encoder_op = layers[i]
+        opt = sess.run(encoder_op['encoder'], feed_dict={inputX: opt})
+    return opt
 
 def create_encoder(weight_e, bias_e, weight_d, bias_d, x):
     encoder = tf.nn.sigmoid(tf.add(tf.matmul(x, weight_e), bias_e))
@@ -36,30 +35,30 @@ def auto_encoder(x, input_size, output_size):
 
     return create_encoder(weight, bias, weight2, bias2, x)
 
-
-
-def train():
-    X = tf.placeholder("float", [None, 784])
-    temp_encoder = auto_encoder(X, 784, 256)
-
-    y_pred = temp_encoder['decoder']
-    y_true = temp_encoder['x']
-
-    cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-    optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-
-
+def train_layer(training_epochs, batch_size, display_step, optimizer, cost, input_data, temp_encoder, layers):
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
 
-        total_batch = int(mnist.train.num_examples / batch_size)
+        total_batch = int(len(input_data) / batch_size)
         for epoch in range(training_epochs):
+            count = 0
+            cst = 0.0
+
             for i in range(total_batch):
-                batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+                end = count + batch_size
+                if end > len(input_data):
+                    end = len(input_data)
+
+                batch_xs = runLayers(input_data[count:end], layers, session)
                 _, c = session.run([optimizer, cost], feed_dict={temp_encoder['x']: batch_xs})
+
+                cst = cst + c
+                count = end
+
+            cst = cst / total_batch
             if epoch % display_step == 0:
                 print("Epoch:", '%04d' % (epoch + 1),
-                      "cost=", "{:.9f}".format(c))
+                      "cost=", "{:.9f}".format(cst))
 
         print("Optimization Finished!")
 
@@ -69,9 +68,29 @@ def train():
         weight_d = tf.constant(session.run(temp_encoder['weightd']))
         bias_d = tf.constant(session.run(temp_encoder['biasd']))
 
-        return create_encoder(weight_e, bias_e, weight_d, bias_d, temp_encoder['x'])
+    return create_encoder(weight_e, bias_e, weight_d, bias_d, temp_encoder['x'])
 
-encoder = train()
+def train(learning_rate, training_epochs, batch_size, display_step, input_data, hidden_size):
+    input_size = len(input_data[1])
+
+    X = tf.placeholder("float", [None, input_size])
+    temp_encoder = auto_encoder(X, input_size, hidden_size)
+
+    y_pred = temp_encoder['decoder']
+    y_true = temp_encoder['x']
+
+    cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+    optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+    return train_layer(training_epochs, batch_size, display_step, optimizer, cost, input_data, temp_encoder, [])
+
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+examples_to_show = 10
+encoder = train(0.01, 20, 256, 1, mnist.train.images, 256)
 
 with tf.Session() as def_session:
     def_session.run(tf.global_variables_initializer())
